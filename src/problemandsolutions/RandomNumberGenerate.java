957b919f6350173b8915e886058cd236598e7154
Ali Rehan Chowdhury <bilkis@Bilkiss-MacBook-Pro.local>
2020-01-16 00:18:26 -0500

    randomNumber

--- src/problemandsolutions/RandomNumberGenerate.java
+++ src/problemandsolutions/RandomNumberGenerate.java
@@ -1,8 +1,21 @@
 package problemandsolutions;
 
+
+import java.util.Random;
+
 public class RandomNumberGenerate {
-    //Write Java program to generate a random number between 1 to 8.
 
-
-
+    public static void main(String[] args) {
+        Random rNum = new Random();
+        int num = 0;
+        while (true) {
+            num = rNum.nextInt(9);
+            if (num != 0) break;
+        }
+        System.out.println(num);
+    }
 }
+
+
+
+
