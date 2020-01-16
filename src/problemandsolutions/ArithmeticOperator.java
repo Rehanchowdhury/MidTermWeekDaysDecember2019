4508ac8680d9270b01fdeb3370c3435842a3130f
Ali Rehan Chowdhury <bilkis@Bilkiss-MacBook-Pro.local>
2020-01-15 22:21:10 -0500

    Arithmetic

--- src/problemandsolutions/ArithmeticOperator.java
+++ src/problemandsolutions/ArithmeticOperator.java
@@ -1,4 +1,5 @@
 package problemandsolutions;
+import java.util.Scanner;
 
 public class ArithmeticOperator {
     //Write Java program to allow the user to input two integer values and then the program prints the results of adding,
@@ -6,9 +7,27 @@ public class ArithmeticOperator {
 
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Input first number: ");
+        int num1 = in.nextInt();
+
+        System.out.println("Input second number: ");
+        int num2 = in.nextInt();
+
+
+        System.out.println("The adding value is " + (num1 + num2));
+
+        System.out.println("The subtract value is " + (num1 - num2));
+
+        System.out.println("The multiply value is " + (num1 * num2));
+
+        System.out.println("The division value is " +   (num1 / num2));
+
+
+
 
     }
-
-
-
 }
+
