5f0c519c4bb5ba44310e624eb5ebc8e252ffc1cd
Ali Rehan Chowdhury <bilkis@Bilkiss-MacBook-Pro.local>
2020-01-15 23:29:41 -0500

    GradeSheet

--- src/problemandsolutions/GradeSheet.java
+++ src/problemandsolutions/GradeSheet.java
@@ -1,13 +1,17 @@
 package problemandsolutions;
 
+import java.util.Scanner;
+
 public class GradeSheet {
+
+
     // Write a Java program that determines a student’s grade.
-    //The program will read three types of scores(quiz, mid-term, and final scores) and determine the grade based on
-    // the following rules:
-    //- if the average score >=90% =>grade=A
-    //- if the average score >= 70% and <90% => grade=B
-    //- if the average score>=50% and <70% =>grade=C
-    //- if the average score<50% =>grade=F
+        // The program will read three types of scores(quiz, mid-term, and final scores) and determine the grade based on
+     //   the following rules:
+    //-  if the average score >=90% =>grade=A
+    //-  if the average score >= 70% and <90% => grade=B
+    //-  if the average score>=50% and <70% =>grade=C
+    //-  if the average score<50% =>grade=F
     //
     //See the example output below:
     //
@@ -21,7 +25,29 @@ public class GradeSheet {
 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your quiz score please");
+                int quizScore=scanner.nextInt();
+        System.out.println("Enter your midterm score please");
+                int midterm=scanner.nextInt();
+        System.out.println("Enter your final  score please");
+                int finalScore=scanner.nextInt();
+                int avg=(quizScore+midterm+finalScore)/3;
 
-    }
 
-}
+                System.out.print("The student Grade is: ");
+                if(avg>=90){
+                    System.out.print("A");
+                } else if(avg>=70 && avg<90){
+                    System.out.print("B");
+                } else if(avg>=50 && avg<70){
+                    System.out.print("C");
+                } else{
+                    System.out.print("F");
+                }
+            }
+        }
+
+
+
+
