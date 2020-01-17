20423b0f54fbafe10989fc50df7f508c7fc0bc2d
Ali Rehan Chowdhury <bilkis@Bilkiss-MacBook-Pro.local>
2020-01-17 04:46:54 -0500

    CollectionView

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +12,37 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		ArrayList<Integer> myArrayList = new ArrayList<>(2);
 
+		myArrayList.add(5);
+		myArrayList.add(10);
+		myArrayList.add(15);
+		myArrayList.add(20);
+	    myArrayList.add(25);
+
+
+		System.out.println("The ArrayList items by using For Each:");
+		for(Integer item:myArrayList)
+			System.out.println(item);
+
+		// Retrieving data using iterator
+
+		System.out.println("The ArrayList items by using iterator interface:");
+
+		Iterator iterator = myArrayList.iterator();
+
+		while(iterator.hasNext())
+			System.out.println(iterator.next());
+
+		myArrayList.remove(Integer.valueOf(20));
+		System.out.println("Removed 20. Now the ArrayList content is:");
+		for(Integer item: myArrayList)
+			System.out.println(item);
+
+		System.out.println("Retrieving element at index 3: " + myArrayList.get(3));
 	}
 
+
 }
+
+
--- src/problemandsolutions/SearchElementOfArray.java
+++ src/problemandsolutions/SearchElementOfArray.java
@@ -7,6 +7,19 @@ public class SearchElementOfArray {
 
     public static void main(String[] args) {
 
+        int array[] = {10, 20, 25, 63, 96, 57};
+        int size = array.length;
+        int value = 20;
+
+        for (int i=0 ;i< size-1; i++){
+            if(array[i]==value){
+                System.out.println("Element found index is :"+ i);
+            }else{
+                System.out.println("Element not found");
+            }
+        }
+
+
 
     }
 }
