a5072a752515ae23f8f8a77eb8fb1d29bdf4a30c
Ali Rehan Chowdhury <bilkis@Bilkiss-MacBook-Pro.local>
2020-01-16 20:57:54 -0500

    Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -1,12 +1,30 @@
 package math.problems;
 
 public class Fibonacci {
-    public static void main(String[] args) {
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
 
+        public static void main(String[] args) {
+            int n = 40, t1 = 0, t2 = 1;
+           System.out.print("First " + n + " terms: ");
 
+            for (int i = 1; i <= n; ++i)
+            {
+                int sum = t1 + t2;
+                t1 = t2;
+                t2 = sum;
+                System.out.print(t1 + " + ");
+            }
+        }
     }
-}
+
+
+
+
+
+
+
+
+
